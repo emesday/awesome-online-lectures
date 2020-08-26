656a6638b16b2941b7e4dc0c594021cad0914452
em.es <em.es@kakaoenterprise.com>
2020-08-26 14:43:54 +0900

    Updates

---
+++ precache-manifest.38a99f72c9c0c61b31bdcd83b3d7c051.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f3f2226477d135c9ddd348fcc41784",
+    "url": "/awesome-online-lectures/index.html"
+  },
+  {
+    "revision": "f0b8bb92d9ab4d565f80",
+    "url": "/awesome-online-lectures/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2df787c5d521fd4d85b0",
+    "url": "/awesome-online-lectures/static/js/2.78497f56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/awesome-online-lectures/static/js/2.78497f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b8bb92d9ab4d565f80",
+    "url": "/awesome-online-lectures/static/js/main.882fd209.chunk.js"
+  },
+  {
+    "revision": "ca57f8a5fce4dffe3555",
+    "url": "/awesome-online-lectures/static/js/runtime-main.348269b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/awesome-online-lectures/static/media/logo.5d5d9eef.svg"
+  }
+]);
